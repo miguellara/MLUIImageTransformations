88cca3a32841ac6852266bc4d90ecc89ca6cd1f0
Miguel Lara <miguel@mac.com>
2014-03-31 19:41:28 +0100

    Initial Commit

---
+++ MLImageFramework/MLImageFramework.h
@@ -0,0 +1,13 @@
+//
+//  MLImageFramework.h
+//  MLImageFramework
+//
+//  Created by Miguel Lara on 31/03/14.
+//  Copyright (c) 2014 Miguel Lara. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MLImageFramework : NSObject
+
+@end
---
+++ MLImageFramework/MLImageFramework.m
@@ -0,0 +1,13 @@
+//
+//  MLImageFramework.m
+//  MLImageFramework
+//
+//  Created by Miguel Lara on 31/03/14.
+//  Copyright (c) 2014 Miguel Lara. All rights reserved.
+//
+
+#import "MLImageFramework.h"
+
+@implementation MLImageFramework
+
+@end
